83796712b257e127ceec495d3e4a984299f36163
aboud20-meet <aboud20@meet.mit.edu>
2018-08-05 16:29:38 +0300

    started python

--- exercises/conditionals.py
+++ exercises/conditionals.py
@@ -1,1 +1,14 @@
 # Write your solution for 1.2 here!
+sum = 0
+# for i in range(101):
+#     if i % 2 == 0:
+#         sum = sum + i
+# print(sum)
+
+for i in range(1000, 0, -1):
+    if i % 6 == 2 and i*i % 5 == 3:
+        print (i)
+        break
+        
+    
+    
--- exercises/for_loops.py
+++ exercises/for_loops.py
@@ -1,1 +1,9 @@
 # Write your solution for 1.1 here!
+sum = 0
+#for i in range(101):
+ #   sum = sum + i
+#print(sum)
+
+for i in range (2, 101, 2):
+   sum = sum + i
+print(sum) 
